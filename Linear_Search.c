75436d44fa28306ef25ced133679e8d86be70d5a
Gaurav Rajpoot <40191477+GauravRajpoot@users.noreply.github.com>
2019-07-14 19:41:35 +0530

    Update Linear_Search.c

--- Linear_Search.c
+++ Linear_Search.c
@@ -1,11 +1,22 @@
+/*Implementation of Linear Search using Array*/
+
 #include<stdio.h>
 int main(){
-int target_value,arr[5],i;
+int target_value,arr[5],i;  
+    
+//Getting the input from user
+    
 printf("Enter the elements of an array :");
 for(i=0;i<5;i++){
 scanf("%d",&arr[i]);}
+
+//Getting the Elemnet to be searched from user
+    
 printf("Enter the elements to be searched :");
 scanf("%d",&target_value);
+ 
+//Calling Linear Search Function
+    
 int index=Linear_Search(arr,5,target_value);
 if(index==0)
     printf("Entered Number is not present");
@@ -13,10 +24,12 @@ else
     printf("The number %d is present at index %d",arr[index],index);
 }
 
+//Linear Search Function Defination
+
 int Linear_Search(int *a,int size,int target){
     for(int i=0;i<size;i++){
         if(a[i]==target)
             return i;
     }
-return 0;
+  return 0;
 }
